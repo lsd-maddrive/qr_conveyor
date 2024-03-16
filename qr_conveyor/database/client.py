0232a8a4cc5284157717c805316ebb5ceec393bd
Vadim Ivanov <vadim-ivanov01@mail.ru>
2024-03-16 19:21:53 +0300

    [#3] издÐÐ

--- qr_conveyor/database/client.py
+++ qr_conveyor/database/client.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, sessionmaker
@@ -11,22 +12,23 @@ class DatabaseClient:
         self._logger = logging.getLogger(__name__)
 
         # Загружаем переменные окружения из файла .env
-        load_dotenv()
+        load_dotenv(override=True)
 
         # Проверяем наличие переменной окружения DB_DPATH
         db_dpath = os.getenv("DB_DPATH")
         if db_dpath is None:
             raise ValueError("Переменная окружения DB_DPATH не определена в файле .env")
 
-        # Проверяем, существует ли папка для БД, и создаем ее, если необходимо
-        if not os.path.exists(db_dpath):
-            os.makedirs(db_dpath)
+        # Переписываем проверку существования папки через pathlib
+        db_path = Path(db_dpath)
+        if not db_path.exists():
+            db_path.mkdir(parents=True)
 
         # Создаем переменную с путем до файла БД
-        self.db_path = os.path.join(db_dpath, "data.db")
+        self.db_path = db_path / "data.db"
 
-        # Создаем объект движка SQLAlchemy
-        self.engine = create_engine("sqlite:///" + self.db_path)
+        # Создаем объект движка SQLAlchemy через f-string
+        self.engine = create_engine(f"sqlite:///{self.db_path}")
 
         # Создаем объект сессии
         self._session = sessionmaker(bind=self.engine)
